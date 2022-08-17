72640bc0ad213b143f706fd4979f75d0ccb5dfef
Mohamed Hassan <mohamedh@Mohameds-MBP.broadband>
2022-08-17 11:49:53 +0100

    amend card-image story

--- src/components/card-image/card-image.stories.tsx
+++ src/components/card-image/card-image.stories.tsx
@@ -7,7 +7,15 @@ export default {
   component: CardImage,
   args: {}
 } as ComponentMeta<typeof CardImage>
-const Template: ComponentStory<typeof CardImage> = () => <CardImage />
+const Template: ComponentStory<typeof CardImage> = () => (
+  <CardImage
+    photo="https://images.unsplash.com/photo-1637998450975-15193338ee93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80"
+    cardTitle="Title"
+    cardText="Text"
+    publishedAt="2022-07-31 18:14"
+    slug="this-is-a-test"
+  />
+)
 
 export const Default = Template.bind({})
 Default.args = {}
