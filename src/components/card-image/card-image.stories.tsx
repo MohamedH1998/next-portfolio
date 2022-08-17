8360351a364138355b406cbe70a250729476f425
Mohamed Hassan <mohamedh@Mohameds-MBP.broadband>
2022-08-17 13:54:45 +0100

    debug 3

--- src/components/card-image/card-image.stories.tsx
+++ src/components/card-image/card-image.stories.tsx
@@ -12,7 +12,7 @@ const Template: ComponentStory<typeof CardImage> = () => (
     photo="https://images.unsplash.com/photo-1637998450975-15193338ee93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80"
     cardTitle="Title"
     cardText="Text"
-    publishedAt="2022-07-31 18:14"
+    timestamp="2022-07-31 18:14"
     slug="this-is-a-test"
   />
 )
